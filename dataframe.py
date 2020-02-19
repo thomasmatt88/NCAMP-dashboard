2202d6a998d8cafa86c8d9ede4e1f261d509fdf7
Matthew Thomas <thomasmatt@me.com>
2020-02-18 17:12:12 -0800

    refactoring via modularization

---
+++ dataframe.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+db_connection_str = 'mysql+pymysql://root:root@localhost:3306/NCAMP'
+db_connection = create_engine(db_connection_str)
+
+df = pd.read_sql('SELECT * FROM material', con=db_connection)
+df1 = pd.read_sql('SELECT * FROM material_properties', con=db_connection)
+df2 = pd.read_sql('SELECT * FROM test_conditions', con=db_connection)
+df3 = pd.merge(df1, df2, how = 'inner', left_on = 'test_conditions_id', right_on = 'id')
+df4 = pd.merge(df, df3, how = 'inner', left_on = 'id', right_on = 'material_id')
+df5 = df4
+
+#df preprocessing
+#join material and material properties by material 'id'
+#drop unneccessary columns 
+df6 = pd.merge(df[['id', 'Material']], df1, how = 'inner', left_on = 'id', right_on = 'material_id')
+df6 = df6.drop(columns = ['id_x', 'id_y'])
+
+# add units header to df and df6
+df = df.rename(columns = {"MOT": "MOT (°F)", "Tg": "Tg (°F)", "WetTg" :"WetTg(°F)", "FAW": "FAW (g/m^2)"})
+"""
+header_tuples = [('Material', None), ('material_id', None), ('test_conditions_id', None), \
+    ('F1tu', 'ksi'), ('F2tu', 'ksi'), ('E1t', 'msi'), ('F1cu', 'ksi'), \
+        ('F2cu', 'ksi'), ('F12su', 'ksi'), ('F31sbs', 'ksi'), ('CPT', 'in/ply')]
+df6.columns = pd.MultiIndex.from_tuples(header_tuples)
+"""
+df6 = df6.rename(columns  = {"F1tu": "F1tu (ksi)", "F2tu": "F2tu (ksi)", "E1t": "E1t (msi)", \
+    "F1cu": "F1cu (ksi)", "F2cu": "F2cu (ksi)", "F12su": "F12su (ksi)", "F31sbs": "F31sbs (ksi)", \
+        "CPT": "CPT (in/ply)"})
---
+++ layout.py
@@ -0,0 +1,80 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+import dash_bootstrap_components as dbc
+from dataframe import df, df6
+
+#this element will contain all our other elements 
+Layout = html.Div([
+    html.H1("NCAMP Table"),
+    html.H3("Select a Material"),
+    dcc.Dropdown(
+        id = 'material-dropdown', #need to reference for callback
+        options=[
+        {'label': 'Hexcel AS4/8552 unidirectional tape', 'value': 1},
+        {'label': 'Hexcel AS4 3k/8552 plain weave', 'value': 2},
+        {'label': 'Hexcel IM7/8552 unidirectional tape', 'value': 3},
+        {'label': 'Newport MR60H/ NCT4708 unidirectional tape', 'value': 4},
+        {'label': 'HTS40 E13 3k/MTM45-1 plain weave fabric', 'value': 5},
+        {'label': '6781 S-2/MTM45-1 8-harness satin weave fabric', 'value': 6},
+        {'label': 'E-Glass 7781/MTM45-1 8-harness satin weave fabric', 'value': 7}
+        ],
+        placeholder="Select a material",
+        multi = True
+    ),
+    html.Div(
+        id = "material-selection", #need to reference for callback
+        ), 
+    dash_table.DataTable(
+        id = 'material-table', #need to reference for callback
+        columns = [{"name": i, "id": i} for i in df.drop(columns = 'id').columns], #don't need id column
+        data = df.drop(columns = 'id').to_dict("rows"),
+        style_header = {
+            'fontWeight': 'bold'
+        }
+    ),
+    html.H3("Select a Filter"),
+    html.Div(
+        [
+            dbc.Button("Test Condition", id="open"),
+            dbc.Modal(
+                [
+                    dbc.ModalHeader("Filter properties by test condition."),
+                    dbc.ModalBody(
+                        dcc.Checklist(
+                            id = 'test-condition-checklist', #need to reference for callback
+                            options = [
+                                {'label': '-65°F, dry', 'value': 1},
+                                {'label': '70°F, dry', 'value': 2},
+                                {'label': '250°F, wet', 'value': 3},
+                                {'label': '250°F, dry', 'value': 4},
+                                {'label': '180°F, dry', 'value': 5},
+                                {'label': '180°F, wet', 'value': 6},
+                                {'label': '200°F, wet', 'value': 7}
+                            
+                            ]
+                        )
+                    ),
+                    dbc.ModalFooter(
+                        dbc.Button("Close", id="close", className="ml-auto")
+                    ),
+                ],
+                id="modal",
+            ),
+        ]
+    ),
+    html.Div(
+        id = "material-property-selection", #need to reference for callback
+    ),
+    dash_table.DataTable(
+        id = 'material-property-table', #need to reference for callback
+        columns = [{"name": i, "id": i} for i in df6.drop(columns = ['material_id']).columns], #don't need id column
+        data = df6.drop(columns = ['material_id']).to_dict("rows"),
+        style_header = {
+            'fontWeight': 'bold'
+        },
+        sort_action = 'custom',
+        sort_mode = 'single',
+        sort_by = []
+    )
+])
