607d54deaeb85a1699dea5ac620e25bbce71ddad
Matthew Thomas <thomasmatt@me.com>
2020-02-21 20:52:29 -0800

    deployment test 4

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import dash
 import dash_bootstrap_components as dbc
 
 #app = dash.Dash(__name__)
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server #need to add this line for heroku deployment
 app.config.suppress_callback_exceptions = True
 
 # index imports app, so import it after app is defined to avoid a circular import
 #from dash_test import index
 
-if __name__ == '__main__':
-    import index
+#if __name__ == '__main__':
+import index
 
 
 
