b126b2f31f0348586251809f0d8de7ffb4829028
Matthew Thomas <thomasmatt@me.com>
2020-02-28 17:07:52 -0800

    move material_table callback to controllers

---
+++ controllers/material_table.py
@@ -0,0 +1,20 @@
+import dash
+from dash.dependencies import Input, Output, State
+
+#from other modules
+from dataframe import material_df
+from app import app
+from views.layout import Layout
+
+@app.callback(
+    Output('material-table', 'data'), #one output id can have one callback
+    [
+        Input('material-dropdown', 'value'),
+        Input('material-table', 'sort_by')
+    ])
+def update_material_table(value, sort_by):
+    dff = material_df
+    if value is None:
+        return dff[dff['id'] == value].to_dict("rows") #one value at a time
+    else:
+        return dff[dff['id'].isin(value)].to_dict("rows")
--- index.py
+++ index.py
@@ -10,33 +10,13 @@ import pymysql
 
 #from other modules
 from views.layout import Layout
-from dataframe import material_df
 from app import app
 import controllers.property_filters
 import controllers.material_property_table
+import controllers.material_table
 
 #this element will contain all our other elements 
 app.layout = Layout
-"""
-#usually place callbacks near bottom of application
-@app.callback(
-    Output('material-selection', 'children'),
-    [Input('material-dropdown', 'value')])
-def update_material_output(value):
-    return 'You have selected "{}"'.format(value)
-"""
-@app.callback(
-    Output('material-table', 'data'), #one output id can have one callback
-    [
-        Input('material-dropdown', 'value'),
-        Input('material-table', 'sort_by')
-    ])
-def update_material_table(value, sort_by):
-    dff = material_df
-    if value is None:
-        return dff[dff['id'] == value].to_dict("rows") #one value at a time
-    else:
-        return dff[dff['id'].isin(value)].to_dict("rows")
     
 @app.callback(
     Output("modal", "is_open"),
