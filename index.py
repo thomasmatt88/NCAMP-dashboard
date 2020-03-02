494f6ec3493dd4cf61f3334369c3efbd1a48978f
Matthew Thomas <thomasmatt@me.com>
2020-03-01 21:30:20 -0800

    created rawdata endpoint

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 #from other modules
 from views.layout import Layout
 from app import app
@@ -5,11 +7,20 @@ import controllers.property_filters
 import controllers.material_property_table
 import controllers.material_table
 import controllers.filter_modals
+from dataframe import material_df, property_df
 
 #this element will contain all our other elements 
 app.layout = Layout
 
 #app.run_server(debug=True)
+
+@app.server.route('/rawdata/<table>')
+def rawdata(table):
+    if table == 'material':
+        return jsonify(material_df.to_json(orient = 'records'))
+    elif table == 'property':
+        return jsonify(property_df.to_json(orient = 'records'))
+    return "Valid path parameters are 'material' or 'property'"
 
 if __name__ == '__main__':
     #don't use debug = True on production server
