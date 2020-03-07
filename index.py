81b80dcd4bca036f7631c90f26e3fe06c563cb92
Matthew Thomas <thomasmatt@me.com>
2020-03-06 18:23:31 -0800

    removed rest api code. did not mean to branch off of rest_api branch

--- index.py
+++ index.py
@@ -1,5 +1,3 @@
-from flask import jsonify, request, redirect
-
 #from other modules
 from views.layout import Layout
 from app import app
@@ -8,29 +6,11 @@ import controllers.physical_property_filters
 import controllers.material_property_table
 import controllers.material_table
 import controllers.filter_modals
-from dataframe import material_df, property_df
 
 #this element will contain all our other elements 
 app.layout = Layout
 
 #app.run_server(debug=True)
-
-@app.server.route('/rawdata/<table>')
-def rawdata(table):
-    if table == 'material':
-        return jsonify(material_df.to_json(orient = 'records'))
-    elif table == 'property':
-        return jsonify(property_df.to_json(orient = 'records'))
-    return "Valid path parameters are 'material' or 'property'"
-
-@app.server.route('/material/<material>', methods = ['GET', 'POST'])
-def addmaterial(material):
-    global material_df
-    if request.method == 'POST':
-        material_df = material_df.append({'Material': material}, ignore_index = True)
-        return jsonify(material_df.to_json(orient = 'records'))
-    return redirect('/')
-
 
 
 if __name__ == '__main__':
