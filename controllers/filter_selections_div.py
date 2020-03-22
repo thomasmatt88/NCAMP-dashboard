19be05783e454858c174b4ada171f3be294dbf45
Matthew Thomas <thomasmatt@me.com>
2020-03-22 15:45:13 -0700

    styled filter_selections_div

--- controllers/filter_selections_div.py
+++ controllers/filter_selections_div.py
@@ -1,5 +1,6 @@
 import dash
 from dash.dependencies import Input, Output
+import dash_html_components as html
 
 #from other modules
 from views.layout import Layout
@@ -34,7 +35,13 @@ def toggle_property_range_modal_phys(n1, n2, n3, n4, n5, n6, n7, *args):
     for i, parameter in enumerate(args):
         mech_props_ranges[i] = parameter
     
-    test_conds = [(test_conditions_df.iloc[i - 1]["temperature"], test_conditions_df.iloc[i - 1]["environment"]) for i in n1]
-    mech_props = [(PropertyDF.PROPERTIES[i], mech_props_ranges[i - 1][0], mech_props_ranges[i - 1][1]) for i in n2]
-    phys_props = [(MaterialDF.PROPERTIES[i], phys_props_ranges[i - 1][0], phys_props_ranges[i - 1][1]) for i in n3]
-    return "Test Conditions: {}, Property Dropdown: {}, Physical Property Dropdown: {}".format(test_conds, mech_props, phys_props)
+    test_conds = ["{}, {}\t".format(test_conditions_df.iloc[i - 1]["temperature"], test_conditions_df.iloc[i - 1]["environment"]) for i in n1]
+    mech_props = ["{}: {}, {}\t".format(PropertyDF.PROPERTIES[i], mech_props_ranges[i - 1][0], mech_props_ranges[i - 1][1]) for i in n2]
+    phys_props = ["{}: {}, {}\t".format(MaterialDF.PROPERTIES[i], phys_props_ranges[i - 1][0], phys_props_ranges[i - 1][1]) for i in n3]
+
+    return html.Div([
+        html.P("".join(test_conds), style = {"color": "#007bff"}),
+        #html.P("".join(mech_props), style = {"color": "#6c757d", "border-style": "solid", "border-width": "1px"}),
+        html.P("".join(mech_props), style = {"color": "#6c757d"}),
+        html.P("".join(phys_props), style = {"color": "#28a745"})
+    ])
