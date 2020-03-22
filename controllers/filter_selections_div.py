9313a23033e6bc1e63e66b95a4027c61b7eacd23
Matthew Thomas <thomasmatt@me.com>
2020-03-22 16:11:20 -0700

    finished styling filter_selections_div

--- controllers/filter_selections_div.py
+++ controllers/filter_selections_div.py
@@ -44,30 +44,26 @@ def toggle_property_range_modal_phys(n1, n2, n3, n4, n5, n6, n7, *args):
                         html.P(test_cond, 
                         style = {"color": "#007bff", "border-style": "solid", \
                                 "border-width": "1px", "display" : "inline-block", \
-                                "margin" : "2px", "padding" : "2px"}) 
+                                "margin" : "2px", "padding" : "2px", "background-color" : "#EEF4FF"}) 
                         for test_cond in test_conds
                     ]
     mech_props_html = [
                         html.P(mech_prop, 
                         style = {"color": "#6c757d", "border-style": "solid", \
                                 "border-width": "1px", "display" : "inline-block", \
-                                "margin" : "2px", "padding" : "2px"})
+                                "margin" : "2px", "padding" : "2px", "background-color" : "#E8E8E8"})
                         for mech_prop in mech_props
                     ]
     phys_props_html = [
                         html.P(phys_prop, 
                         style = {"color": "#28a745", "border-style": "solid", \
                                 "border-width": "1px", "display" : "inline-block", \
-                                "margin" : "2px", "padding" : "2px"})
+                                "margin" : "2px", "padding" : "2px", "background-color" : "#d7fcdf"})
                         for phys_prop in phys_props
                     ]
 
     return html.Div([
-        #html.P("".join(test_conds), style = {"color": "#007bff"}),
         *test_conds_html,
-        #html.P("".join(mech_props), style = {"color": "#6c757d", "border-style": "solid", "border-width": "1px"}),
-        #html.P("".join(mech_props), style = {"color": "#6c757d"}),
         *mech_props_html,
-        #html.P("".join(phys_props), style = {"color": "#28a745"})
         *phys_props_html
-    ])
+    ], style = {"margin-top" : "35px"})
